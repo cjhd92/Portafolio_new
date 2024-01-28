65147520947ac6f829c1ce3d49c6cd7d042f031c
cjhd92 <cjhd92@gmail.com>
2024-01-28 21:17:38 +0100

    seccion_comenzar_finalizada

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import Navbar from './components/Navbar';
-import Comenzar from './components/Comenzar';
-import Contacto from './components/Contacto';
+import Navbar from "./components/Navbar";
+import Comenzar from "./components/Comenzar";
+import Contacto from "./components/Contacto";
+
 
 import './App.css';
 
-
 function App() {
-  return (<>  
-  <h1>Hola hello</h1>
-  <Navbar />
-  <Comenzar />
-  <Contacto />
-  </>
+  return (
+    <>
+    
+    <div className="contenedor-header">
+        <header>
+          <div className='logo'>
+
+              <a href='#'>Grupo</a>
+
+          </div>
+
+          <Navbar />
+        
+      </header>
+      
+      </div>
+
+     
+       <Comenzar/>
+
+       <Contacto />
+
+
+      
+     
+
+
+    
+ 
+      
+      
+    </>
+      
+   
   );
 }
 
